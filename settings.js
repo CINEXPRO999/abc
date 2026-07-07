4347c8e0b53e34ac182a731f5d5774541412ce6d
CINEXPRO999 <cinexpro999@gmail.com>
2026-07-07 16:08:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=FQ0w3B4A#wK9qkz9V-At30wTowvAGUjVJOzZtV1LzkAqd-i2sgzg' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=UN8kyAza#EPoHuIp4HrFcT7Ie0gHIwGHatkYGTonN7GJxMQZR98k' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
